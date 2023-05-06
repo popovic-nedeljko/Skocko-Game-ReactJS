148d55d8528afa257fc64c6c9198982a87c768d1
popovic-nedeljko <nftnedja@gmail.com>
2023-05-06 02:26:11 +0200

    reconstructure of code - dynamic

--- src/components/SecretSymbols/SecretSymbols.js
+++ src/components/SecretSymbols/SecretSymbols.js
@@ -8,26 +8,14 @@ function SecretSymbol() {
 
   return (
     <div className='secret-symbol'>
-      <SingleItem
-        item={secretSymbols[0]}
-        symbol={`${secretSymbols[0]?.type.name}`}
-        hidden={hidden ? 'hidden' : ''}
-      />
-      <SingleItem
-        item={secretSymbols[1]}
-        symbol={secretSymbols[1]?.type.name}
-        hidden={hidden ? 'hidden' : ''}
-      />
-      <SingleItem
-        item={secretSymbols[2]}
-        symbol={secretSymbols[2]?.type.name}
-        hidden={hidden ? 'hidden' : ''}
-      />
-      <SingleItem
-        item={secretSymbols[3]}
-        symbol={secretSymbols[3]?.type.name}
-        hidden={hidden ? 'hidden' : ''}
-      />
+      {secretSymbols.map((symbol, index) => (
+        <SingleItem
+          key={index}
+          item={symbol}
+          symbol={`${symbol?.type.name}`}
+          hidden={hidden ? 'hidden' : ''}
+        />
+      ))}
     </div>
   );
 }
