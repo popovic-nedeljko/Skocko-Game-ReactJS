9d35091b59e6ce0f68d8836d9b3e739990c8f085
popovic-nedeljko <nftnedja@gmail.com>
2023-05-06 19:47:40 +0200

    reconstructure of code - dynamic

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -20,13 +20,20 @@ function Navbar() {
     renderRandomSymbols();
     setHidden(true);
   };
+
   return (
     <div className='navbar'>
       <Link to={'about'} className='nav-link about'>
         ABOUT
       </Link>
       <h1 className='title'>SKOČKO QUIZ GAME</h1>
-      <Link to={'/'} className='nav-link new-game' onClick={handleClick}>
+      <Link
+        to={'/'}
+        className='nav-link new-game'
+        onClick={() => {
+          window.location.href = '/';
+        }}
+      >
         NEW GAME
       </Link>
     </div>
